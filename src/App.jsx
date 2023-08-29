08144f42deb92ae4e6911f40aefadb1574ee2815
ZekeScript <estiga27@gmail.com>
2023-08-28 23:03:55 -0300

    add button for get new fact

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ function App () {
   const [fact, setFact] = useState()
   const [imgUrl, setImgUrl] = useState()
 
-  useEffect(() => {
+  const getFact = () => {
     fetch(RANDOM_FACT_ENDPOINT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
       })
-  }, [])
+  }
+
+  useEffect(getFact, [])
 
   useEffect(() => {
     if (!fact) return
 
-    const firstWord = fact.split(' ').slice(0, 1)
+    const firstWord = fact.split(' ', 1)
 
     fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
       .then(res => res.json())
@@ -30,10 +32,15 @@ function App () {
       })
   }, [fact])
 
+  const handleClick = () => {
+    getFact()
+  }
+
   return (
     <main>
       <h1>CatWord</h1>
       {fact && <p>{fact}</p>}
+      <button onClick={handleClick}>Get new fact</button>
       {imgUrl && <img src={CAT_IMG_ENDPONT + imgUrl} />}
     </main>
   )
